442e2354476b42a698f5d89087f1f4521d0d3304
fufufukakaka <KuriMac@Yukihisa-no-iMac.local>
2015-11-14 16:35:46 +0900

    遺伝アルゴリズム実装できた
    データベース設計もこなしたった。
    またartisocに戻ります

--- trigger.sql
+++ trigger.sql
@@ -5,6 +5,7 @@ g3 text;
 g4 text;
 gm1 text;
 gm2 text;
+genecheck integer;
 rand1 integer;
 rand2 integer;
 gmu1 text;
@@ -13,7 +14,6 @@ characters text;
 randgene text;
 r integer;
 i integer;
-tempo text;
 ttt integer;
 beginpoint integer;
 mubegin integer;
@@ -25,30 +25,61 @@ mupl integer;
 crossrate numeric;
 mutationrate numeric;
 sumfitness numeric;
-
+minfitness numeric;
+avgfitness numeric;
+maxfitness numeric;
 
 BEGIN
+-- estimatedgeneにinsertされたオブジェクトを取得する
+-- 取得したオブジェクトを対応するeidでgeneboardにUPDATEする
+UPDATE geneboard set gene = eval.gene,fitness = eval.fitness,
+oldgeneration = eval.oldgeneration  from 
+(select eid,gene,fitness,oldgeneration from estimatedgene where estimatedgene.eid = new.eid) as eval 
+where geneboard.eid = eval.eid;
+-- 重複していないならgenelistにgene,generation,fitnessをinsertする
+select count(gene) into genecheck from genelist 
+where genelist.gene = (select gene from estimatedgene where estimatedgene.eid = new.eid);
+if genecheck = 0 then
+INSERT into genelist(gene,generation,fitness) 
+select new.gene,new.oldgeneration,new.fitness from estimatedgene where estimatedgene.eid = new.eid;
+end if;
+
+-- 適応度をスケーリングする
+select max(fitness) into maxfitness from geneboard;
+select min(fitness) into minfitness from geneboard;
+select avg(fitness) into avgfitness from geneboard;
+UPDATE geneboard set scalefitness = 
+(avgfitness/(avgfitness - minfitness))*fitness - ((minfitness * avgfitness)/(avgfitness - minfitness)); 
+UPDATE geneboard set scalefitness = 0 where scalefitness < 0;
+
 -- Probability Calculation and Cumulation
-select sum(fitness) into sumfitness from newgeneration;
-UPDATE newgeneration set probability = fitness / sumfitness;
-UPDATE newgeneration set cumlativeprobab = x.probab from
- (select geneid,(select sum(probability) from newgeneration where geneid <= x.geneid)
- as probab from newgeneration as x order by geneid) as x  where newgeneration.geneid = x.geneid;
--- Roulette Choice(48)
-for r in 1 .. 20
+UPDATE geneboard set newgeneration = oldgeneration + 1;
+select sum(scalefitness) into sumfitness from geneboard;
+UPDATE geneboard set probability = scalefitness / sumfitness;
+UPDATE geneboard set cumlativeprobab = x.probab from
+ (select eid,(select sum(probability) from geneboard where eid <= x.eid)
+ as probab from geneboard as x order by eid) as x  where geneboard.eid = x.eid;
+-- Roulette Choice(28)
+for r in 1 .. 28
 LOOP	
-	select gene into tempo from newgeneration where cumlativeprobab <= random() order by cumlativeprobab desc limit 1;
-	UPDATE randtable set unified = tempo,generation = oldgene where eid = r;
+	UPDATE randtable set unified = tempo.gene,newgeneration =  tempo.newgeneration 
+	from (select gene,newgeneration from geneboard 
+	where cumlativeprobab <= random() order by cumlativeprobab desc limit 1) as tempo
+	where randtable.eid = r;
 end loop;
 -- Elite Choice(2)
-UPDATE elite set elitegene = x.elite, oldgene = x.oldg from 
-(select generation as oldg,gene as elite from newgeneration order by fitness desc limit 2) as x where id <= 2;
+UPDATE elitetable set elite = x.gene,newgeneration = x.newg from
+(select newgeneration as newg ,gene from geneboard where fitness < (select max(fitness) from geneboard) limit 1)
+as x where elitetable.eid = 29;
+UPDATE elitetable set elite = x.gene,newgeneration = x.newg from
+(select newgeneration as newg ,gene from geneboard where fitness = (select max(fitness) from geneboard))
+as x where elitetable.eid = 30;
 
 -- Crossing
-for cr in 1 .. 10
+for cr in 1 .. 14
 LOOP
 crossrate = random();
-ttt = round(random()*9) + 1;
+ttt = round(random()*49) + 1;
 beginpoint = 2 * ttt - 2;
 endpoint = 2 * ttt - 1;
 if crossrate >= 0.8 then
@@ -59,11 +90,11 @@ if crossrate >= 0.8 then
 	UPDATE randtable set newsons = g3 where eid = 2*cr;
 else
 -- tttの位置に合わせて交差する
-if ttt >= 2 and ttt <= 9 then
+if ttt >= 2 and ttt <= 49 then
 	select substr(unified,1,beginpoint) into g1 from randtable where eid = (2*cr - 1);
-	select substr(unified,endpoint,(20 - endpoint + 1)) from randtable into g2 where eid = (2*cr - 1);
+	select substr(unified,endpoint,(100 - endpoint + 1)) from randtable into g2 where eid = (2*cr - 1);
 	select substr(unified,1,beginpoint) from randtable into g3 where eid = 2*cr;
-	select substr(unified,endpoint,(20 - endpoint + 1)) from randtable into g4 where eid = 2*cr;
+	select substr(unified,endpoint,(100 - endpoint + 1)) from randtable into g4 where eid = 2*cr;
 	UPDATE randtable set newsons = (g1 || g4) where eid = (2*cr - 1);
 	UPDATE randtable set newsons = (g3 || g2) where eid = 2*cr;
 elsif ttt = 1 then
@@ -71,7 +102,7 @@ elsif ttt = 1 then
 	select unified into g4 from randtable where eid = 2*cr;
 	UPDATE randtable set newsons = g4 where eid = (2*cr - 1);
 	UPDATE randtable set newsons = g2 where eid = 2*cr;
-elsif ttt = 10 then
+elsif ttt = 50 then
 	select unified into g1 from randtable where eid = (2*cr - 1);
 	select unified into g3 from randtable where eid = 2*cr;
 	UPDATE randtable set newsons = g1 where eid = (2*cr - 1);
@@ -82,7 +113,7 @@ end loop; -- 全個体に関する交叉終了
 
 -- mutation
 -- 交叉と考え方は同じで、突然変異が起こる場所が決まったら遺伝子を3つ(突然変異以前、突然変異、突然変異以後)に分解して、突然変異部分を生成し、くっつける
-for mu in 1 .. 20
+for mu in 1 .. 28
 loop
 -- 突然変異するならば次の遺伝子を挿入する
 rand1 = round(random()*19)+1;
@@ -93,7 +124,7 @@ gmu2 = substr(characters,rand2,1);
 randgene = (gmu1 || gmu2);
 mutationrate = random();
 -- define mutation place
-mupl = round(random() * 9) + 1;
+mupl = round(random() * 48) + 1;
 mubegin = 2*(mupl - 1);
 muend = (2*mupl + 1);
 -- テストのため、0.8に設定して挙動を見る
@@ -103,19 +134,27 @@ muend = (2*mupl + 1);
 	   UPDATE randtable set newsonsmutation = gm1 where eid = mu;
     else
     -- 突然変異率を満たすならば、該当箇所に応じた変異処理をする
-        if mupl >= 2 and mupl <= 9 then
+        if mupl >= 2 and mupl <= 49 then
             select substr(newsons,1,mubegin) into gm1 from randtable where eid = mu;
-            select substr(newsons,muend,(20 - muend + 1)) into gm2 from randtable where eid = mu;
+            select substr(newsons,muend,(100 - muend + 1)) into gm2 from randtable where eid = mu;
             UPDATE randtable set newsonsmutation = (gm1 || randgene || gm2) where eid = mu;
         elsif mupl = 1 then
-            select substr(newsons,3,18) into gm2 from randtable where eid = mu;
+            select substr(newsons,3,98) into gm2 from randtable where eid = mu;
             UPDATE randtable set newsonsmutation = (randgene || gm2) where eid = mu;
-        elsif mupl = 10 then
-            select substr(newsons,1,18) into gm1 from randtable where eid = mu;
+        elsif mupl = 50 then
+            select substr(newsons,1,98) into gm1 from randtable where eid = mu;
             UPDATE randtable set newsonsmutation = (gm1 || randgene) where eid = mu;
         end if;
 	end if;
 end loop;
+
+-- requestgeneのeidに対応するところにgene,generationをUPDATEする
+UPDATE requestgene set gene = roulettelist.gene,newgeneration = roulettelist.newgeneration from
+(select eid,newsonsmutation as gene,newgeneration from randtable) as roulettelist 
+where requestgene.eid = roulettelist.eid;
+UPDATE requestgene set gene = elitelist.elite,newgeneration = elitelist.newgeneration from
+(select eid,elite,newgeneration from elitetable) as elitelist 
+where requestgene.eid = elitelist.eid;
 
 return new;
 end;
